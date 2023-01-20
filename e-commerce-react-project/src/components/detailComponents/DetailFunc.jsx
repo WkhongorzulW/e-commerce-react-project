92191b5383f3a2a10a41b7317141a8026a5bc0c7
WkhongorzulW <bkhongorzul064@gmail.com>
2023-01-20 13:32:28 +0800

    detail page

--- e-commerce-react-project/src/components/detailComponents/DetailFunc.jsx
+++ e-commerce-react-project/src/components/detailComponents/DetailFunc.jsx
@@ -1,84 +1,37 @@
 import React, { useState } from "react";
-import {
-  contact,
-  header,
-  footerChildren,
-  footerTop,
-  mainFooter,
-  popularProducts,
-} from "../Data";
-import ContactFunc from "../header/ContactFunc";
-import HeaderFunc from "../header/HeaderFunc";
-import MainMenu from "../header/MainMenu";
-import DetailProductFunc from "./playGameDetail";
 import { customer, relatedCam, relatedOther } from "./DetailData";
 import ReviewsFunc from "./Reviews";
 import RelatedCamFunc from "./RelatedCamFunc";
 import { PopularProductsFunc } from "../products/PopularProducts";
-import { FooterTopFunc } from "../footer/FooterTopFunc";
-import { MainFooterFunc, FooterNavFunc } from "../footer/MainFooterFunc";
-import FooterChildrenFunc from "../footer/FooterChidren";
+import { Rating } from "react-simple-star-rating";
+import { siZes } from "./DetailData";
 import { useLocation, useParams } from "react-router-dom";
+
+const sizes = siZes.map((size) => {
+  return <Sizes si={size.si} />;
+});
+
+function Sizes(props) {
+  return <button className="col-2 mx-2 border-0">{props.si}</button>;
+}
 
 function DetailFunc(props) {
   const [wishList, setWishList] = useState([]);
+  const [heart, setHeart] = useState(false);
 
-  const contactSec = contact.map((name) => {
-    return (
-      <ContactFunc
-        help={name.help}
-        store={name.store}
-        delivery={name.delivery}
-      />
-    );
-  });
+  const { id } = useParams();
+  const location = useLocation();
+  const detail = location.state;
+  console.log(detail);
 
-  const headerSec = header.map((item) => {
-    return (
-      <HeaderFunc
-        eLogo={item.eLogo}
-        searchInput={item.searchInput}
-        search={item.search}
-        signIn={item.signIn}
-        fav={item.fav}
-        basket={item.basket}
-        wishList={wishList}
-        window={window}
-        // setWindow={setWindow}
-      />
-    );
-  });
+  function popProductHandler(props) {
+    setHeart(!heart);
+  }
 
-  // const detailProduct = popularProducts.map((product) => {
-  //   return (
-  //     <DetailProductFunc
-  //       id={product.id}
-  //       productImage={product.productImage}
-  //       productName={product.productName}
-  //       price={product.price}
-  //       moreImage={product.moreImage}
-  //       review={product.review}
-  //       available={product.available}
-  //       inStock={product.inStock}
-  //       items={product.items}
-  //       color={product.color}
-  //       colorCircle={product.colorCircle}
-  //       size={product.size}
-  //       quantity={product.quantity}
-  //       quant={product.quant}
-  //       addBtn={product.addBtn}
-  //       buyBtn={product.buyBtn}
-  //       heart={product.heart}
-  //       fullHeart={product.fullHeart}
-  //       sku={product.sku}
-  //       category={product.category}
-  //       share={product.share}
-  //       descBtn={product.descBtn}
-  //       revBtn={product.revBtn}
-  //       checkIcon={product.checkIcon}
-  //     />
-  //   );
-  // });
+  const [rating, setRating] = useState(0);
+  const handleRating = (rate) => {
+    setRating(rate);
+  };
 
   const reviews = customer.map((item) => {
     return (
@@ -119,77 +72,147 @@ function DetailFunc(props) {
     );
   });
 
-  const fTop = footerTop.map((item) => {
-    return (
-      <FooterTopFunc
-        title={item.title}
-        email={item.email}
-        emailIcon={item.emailIcon}
-        hPhoneIcon={item.hPhoneIcon}
-        text={item.text}
-        phNumber={item.phNumber}
-      />
-    );
-  });
-
-  const mFooter = mainFooter.map((item) => {
-    return (
-      <MainFooterFunc
-        logo={item.logo}
-        address={item.address}
-        social={item.social}
-      />
-    );
-  });
-
-  const fNavWithChildren = footerChildren.map((child) => {
-    const children = child.data.map((d) => {
-      return <FooterChildrenFunc title={d.title} />;
-    });
-    return (
-      <div className="col fs-4">
-        <FooterNavFunc title={child.header} />
-        {children}
-      </div>
-    );
-  });
-
   return (
-    <div className="row modal">
-      <div>
-        <div closeButton></div>
+    // <div className="row modal">
+    //   <div>
+    //     <div className="mt-3">
+    //       <div className="row inner-box mx-auto my-modal">
+    //         <h1>Hi {id}</h1>
+    //         <div className="col me-3">
+    //           <div className="row border rounded py-5">
+    //             <img
+    //               className="col-10 prod-img d-block mx-auto my-5"
+    //               src={detail.productImage}
+    //             ></img>
+    //           </div>
+    //           <div className="row mt-3">
+    //             <div className="col border more-img me-3 py-2">
+    //               <img
+    //                 className="col-6 mx-auto d-block"
+    //                 src={detail.moreImage}
+    //               />
+    //             </div>
+    //             <div className="col border more-img py-2">
+    //               <img
+    //                 className="col-6 mx-auto d-block"
+    //                 src={detail.moreImage}
+    //               />
+    //             </div>
+    //           </div>
+    //         </div>
 
-        <div className="m-0 p-0">
-          <div>
-            {contactSec}
-            {headerSec}
-          </div>
-          <div className="menu-container">
-            <MainMenu className="inner-box" />
-          </div>
-          <div className="mt-3">
-            <DetailProductFunc />
-          </div>
-          <div>{reviews}</div>
-          <div className="inner-box mt-5 mb-4">
-            <h3 className="blue-2 fw-bold">Related product</h3>
-          </div>
-          <div className="row inner-box mx-auto mb-5">
-            {relatedC}
-            {relatedO}
-          </div>
-          <div className="footer blue-6 m-0">
-            <div className="footer-top inner-box mx-auto">{fTop}</div>
+    //         <div className="col">
+    //           <h2 className="blue-1">{detail.productName}</h2>
+    //           <div className="back-3 fw-bold fs-3 my-4">{detail.price}</div>
+    //           <div className="fw-semibold black-4">
+    //             <Rating onClick={handleRating} /> {detail.review}
+    //           </div>
+    //           <div className="black-5 my-4">
+    //             {detail.available}{" "}
+    //             <span className="green-1 ms-3">
+    //               {" "}
+    //               {detail.checkIcon} {detail.inStock}
+    //             </span>
+    //           </div>
+    //           <div className="black-6 mb-4">{detail.items}</div>
+    //           <hr />
+    //           <div className="black-5 fw-semibold mt-4">
+    //             {detail.color}{" "}
+    //             <button className="green-2 bg-white rounded-circle mx-2">
+    //               {" "}
+    //               {detail.colorCircle}
+    //             </button>{" "}
+    //             <button className="border-0 bg-white">
+    //               {" "}
+    //               {detail.colorCircle}
+    //             </button>
+    //           </div>
+    //           <div className="row my-4">
+    //             <div className="black-5 fw-semibold col-2">{detail.size}</div>
+    //             <div className="col">{sizes}</div>
+    //           </div>
+    //           <div className="row quant">
+    //             <div className="black-5 fw-semibold col-2">
+    //               {detail.quantity}
+    //             </div>
+    //             <button className="col-1">-</button>
+    //             <button className="black-5 fw-semibold col-2">
+    //               {detail.quant}
+    //             </button>
+    //             <button className="col-1">+</button>
+    //           </div>
+    //           <div className="row my-4 fs-4">
+    //             <button className="orange col-4 me-3 fw-semibold detailBtn mx-auto">
+    //               {detail.addBtn}
+    //             </button>
+    //             <button className="orange col-4 detailBtn fw-semibold mx-auto">
+    //               {detail.buyBtn}
+    //             </button>
+    //             <button
+    //               className="col fs-1 border-0 rounded-circle text-center heartT"
+    //               onClick={() => {
+    //                 popProductHandler(detail);
+    //               }}
+    //             >
+    //               {heart ? detail.heart : detail.fullHeart}
+    //             </button>
+    //           </div>
+    //           <hr />
+    //           <div className="mt-4">
+    //             <div className="black-5 fw-semibold">{detail.sku}</div>
+    //             <div className="my-4 black-5 fw-semibold">
+    //               {detail.category}
+    //             </div>
+    //             <div className="black-5 fw-semibold">
+    //               Share: <img src={detail.share} />
+    //             </div>
+    //           </div>
+    //         </div>
+    //         <div className="row detail-bottom my-4">
+    //           <div className="col-4"></div>
+    //           <button className="col-2 bg-white border fw-semibold fs-5">
+    //             {detail.descBtn}
+    //           </button>
+    //           <button className="col-2 text-white fw-semibold fs-5">
+    //             {detail.revBtn}
+    //           </button>
+    //         </div>
+    //       </div>
+    //     </div>
+    //     <div>{reviews}</div>
+    //     <div className="inner-box mt-5 mb-4">
+    //       <h3 className="blue-2 fw-bold">Related product</h3>
+    //     </div>
+    //     <div className="row inner-box mx-auto mb-5">
+    //       {relatedC}
+    //       {relatedO}
+    //     </div>
+    //   </div>
+    // </div>
 
-            <div className="main-footer inner-box mx-auto row mt-5">
-              <div className="col-2">{mFooter}</div>
-              <div className="col-1"></div>
-              <div className="col footer-nav">
-                <div className="row">{fNavWithChildren}</div>
-              </div>
-            </div>
+    <div>
+      <div className="row">
+        <div className="col">
+          <h1>Product {id}</h1>
+          {/* <div className="row">
+            <img src={detail.productImage} alt="product image" />
           </div>
+          <div className="row">
+            <img src={detail.moreImage} alt="more image" />
+            <img src={detail.moreImage} alt="more image" />
+          </div> */}
         </div>
+
+        <div className="col">
+          {/* <h1>{detail.productName}</h1>
+          <h2>{detail.price}</h2> */}
+        </div>
+      </div>
+      <div>{reviews}</div>
+      <div className="row inner-box mx-auto mb-5">
+        <h2 className="my-4">Related product</h2>
+        {relatedC}
+        {relatedO}
       </div>
     </div>
   );
